047e32346ec196b635e06673a860317befcd0094
AS <albrecht.stephanie@outlook.fr>
2019-10-28 21:59:43 +0100

    correctif + js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('lastname').addEventListener('blur',lostFocus);
+function lostFocus(){
+    alert("Merci de votre participation");
+}
+//récupère le champ lastname et lui ajout un évèment blur (perte de focus), déclenche une alert lorque l'on click hors du champ
